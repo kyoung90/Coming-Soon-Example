ae984a7a58a1354a8e5082c98216d070e7cea2c7
kyoung90 <kenneth.young90@gmail.com>
2020-01-20 15:01:05 -0400

    Added email validation to form and border danger

---
+++ index.js
@@ -0,0 +1,26 @@
+let form = document.querySelector("#form");
+let textBox = document.querySelector(".rounded-btn");
+
+let validEmail = email => {
+    if (/(.+)@(.+){2,}\.(.+){2,}/.test(textBox.value)) {
+      // valid email
+      return true
+    } else {
+      // invalid email
+      return false
+    }
+  };  
+
+let submit = (event) => {
+    event.preventDefault()
+  if (validEmail()) {
+    if (textBox.classList.contains("border-danger")) {
+      textBox.classList.remove("border-danger");
+    }
+    // some logic to store user's email
+  } else {
+    textBox.className += " border-danger";
+  } 
+};
+
+form.onsubmit = submit
