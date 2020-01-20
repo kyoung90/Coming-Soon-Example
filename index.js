b36c907e0132e342edccb1777b5e7867335d2c7f
kyoung90 <kenneth.young90@gmail.com>
2020-01-20 19:12:57 -0400

    Error message successfully shows up on invalid email

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
 let form = document.querySelector("#form");
 let textBox = document.querySelector(".rounded-btn");
+let errorMessage = document.querySelector(".user-email-error");
 
 let validEmail = email => {
-    if (/(.+)@(.+){2,}\.(.+){2,}/.test(textBox.value)) {
-      // valid email
-      return true
-    } else {
-      // invalid email
-      return false
-    }
-  };  
+  if (/(.+)@(.+){2,}\.(.+){2,}/.test(textBox.value)) {
+    // valid email
+    return true;
+  } else {
+    // invalid email
+    return false;
+  }
+};
 
-let submit = (event) => {
-    event.preventDefault()
+let submit = event => {
+  event.preventDefault();
   if (validEmail()) {
+    if (errorMessage.classList.contains("visible")) {
+      errorMessage.classList.toggle("visible");
+    }
+
     if (textBox.classList.contains("border-danger")) {
       textBox.classList.remove("border-danger");
     }
     // some logic to store user's email
   } else {
-    if (!textBox.classList.contains("border-danger")){
-        textBox.className += " border-danger";
+    if (!errorMessage.classList.contains("visible")) {
+      errorMessage.classList.toggle("visible");
     }
-  } 
+
+    if (!textBox.classList.contains("border-danger")) {
+      textBox.className += " border-danger";
+    }
+  }
 };
 
-form.onsubmit = submit
+form.onsubmit = submit;
